1af644d27a366c898d6bd85c0726a6ea7a6de40d
Josh T <joshliam09@gmail.com>
2021-12-01 06:33:14 -0700

    day1 complete

--- day1.py
+++ day1.py
@@ -1,12 +1,20 @@
 data = open('input1.txt', 'r').read().split('\n')
 
 # part 1
-larger = 1 # hehehe
-previous = data[0]
+larger = 0
+previous = int(data[0])
 for i in range(1, len(data)):
-    if previous < data[i]:
+    if previous < int(data[i]):
         larger += 1
-    previous = data[i]
+    previous = int(data[i])
 print(larger)
 
 # part 2
+larger = 0
+previousWindow = int(data[0]) + int(data[1]) + int(data[2])
+for i in range(1, len(data)-2):
+    currWindow = int(data[i]) + int(data[i+1]) + int(data[i+2])
+    if currWindow > previousWindow:
+        larger += 1
+    previousWindow = currWindow
+print(larger)
